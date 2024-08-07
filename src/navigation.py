824b34a137e0a1831d9c37230c1d5725dc31e47b
57Virinchi27 <happyvirinchi@gmail.com>
2024-08-07 18:06:06 +0530

    navigation

---
+++ src/navigation.py
@@ -0,0 +1,60 @@
+import cv2
+import google.generativeai as genai
+from text_to_speech import speak
+
+def navigate():
+    # Capture an image from the webcam
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("DEBUG -- Error: Could not open webcam.")
+        exit()
+
+    ret, frame = cap.read()
+    if ret:
+        # Display the captured image
+        cv2.imshow('DEBUG -- Captured Image', frame)
+        cv2.waitKey(5000)  # Wait for 5 seconds
+        cv2.destroyAllWindows()
+
+        # Convert the frame to image bytes
+        _, img_encoded = cv2.imencode('.jpg', frame)
+        image_bytes = img_encoded.tobytes()
+        cap.release()
+
+        # Configure Google Generative AI
+        api_key = "AIzaSyCi5EYAd8kASk2UQG-BZAIugmOHieQmLUw"  # Replace with your actual API key
+        genai.configure(api_key=api_key)
+        generation_config = {"temperature": 0.9, "top_p": 1, "top_k": 1, "max_output_tokens": 2048}
+        model = genai.GenerativeModel("gemini-1.5-flash", generation_config=generation_config)
+
+        # Create prompt parts
+        prompt_text = (
+            "You are DIMA AI (Digital Instant Mapping Artificial Intelligence), situated on a spectacle set equipped with a camera. "
+            "Your purpose is to assist visually impaired individuals in navigation. Avoid giving multiple instructions at once (max 3)"
+            "The statement in quotes (e.g. 'abcabc') is what the blind person is asking you"
+            "Give commands to the blind person like go forward, turn left, turn right, stop, etc. Give only necessary info."
+            "The blind person asks: 'Guide me. The attached image is what is in fron of me. Please tell me where to go' "
+        )
+        
+        image_part = {
+            "mime_type": "image/jpeg",
+            "data": image_bytes
+        }
+
+        print("DEBUG -- Thinking")
+        speak("Thinking")
+
+        # Generate content
+        response = model.generate_content([prompt_text, image_part])
+
+        # Print the response
+        print(response.text)
+        speak(response.text)
+
+    # If any errors, printing the errors
+    else:
+        print("DEBUG -- Error: Could not read frame from webcam.")
+        speak("Could not read frame from webcam.")
+        cap.release()
+
+navigate()
